0879ae3ef8eff2b66ab621d57b216381b70f57cf
Szymon Maślanka <masslany@gmail.com>
2023-06-12 21:07:10 +0900

    version update

--- Casks/spotify-notify.rb
+++ Casks/spotify-notify.rb
@@ -1,8 +1,8 @@
 cask "spotify-notify" do
-  version "0.4.2"
+  version "0.4.3"
   sha256 :no_check
 
-  url "https://github.com/nahive/spotify-notify/releases/download/0.4.2/Notify.app.zip"
+  url "https://github.com/nahive/spotify-notify/releases/download/0.4.3/Notify.app.zip"
   name "Spotify Notify"
   desc "macOS notifications for Spotify"
   homepage "https://github.com/nahive/spotify-notify"
