098343bb2078a7a06ccdf1701bd3519adf334aac
Szymon Maślanka <masslany@gmail.com>
2024-03-20 19:09:22 +0900

    update hash

--- Casks/spotify-notify.rb
+++ Casks/spotify-notify.rb
@@ -1,6 +1,6 @@
 cask "spotify-notify" do
   version "0.5"
-  sha256 "f4c0b173c85bf48f64c6fbe43028395202e1b1d0c45e9ad32e07e71ea6a4c089"
+  sha256 "2e137be5cdba4577070b34fb4aad4d7de4f0962ec90b8b10f932382403faba94"
 
   url "https://github.com/nahive/spotify-notify/releases/download/#{version}/Notify.zip"
   name "Spotify Notify"
