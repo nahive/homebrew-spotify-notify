d536cad8b6de53aa6df4b9d7e76469aa2d60992b
Szymon Maślanka <masslany@gmail.com>
2025-01-07 18:17:10 +0900

    0.5.2

--- Casks/spotify-notify.rb
+++ Casks/spotify-notify.rb
@@ -1,6 +1,6 @@
 cask "spotify-notify" do
-  version "0.5.1"
-  sha256 "bf7b2b08d267d4fabe4c87fdafd74bb284a5a6563b851f96e3e0b967f36833b0"
+  version "0.5.2"
+  sha256 "2e059dce1e63dce32484a5273735dc82d1058930a33cade8bfb15012854c0a35"
 
   url "https://github.com/nahive/spotify-notify/releases/download/#{version}/Notify.zip"
   name "Spotify Notify"
